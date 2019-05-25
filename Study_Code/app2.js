3f092b7fecd9228055206d35f6306051056a86ee
mycool0905 <mycool0905@gmail.com>
2019-05-25 23:06:04 +0900

    2019-05-25

---
+++ Study_Code/app2.js
@@ -0,0 +1,17 @@
+var express = require('express')
+, http = require('http');
+
+var app = express();
+
+app.use(function(req, res, next){
+    console.log('첫 번째 미들웨어에서 요청을 처리함.');
+    
+    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+    res.end('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+});
+
+app.set('port',process.env.PORT || 2402);
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express 서버가 %d번 포트에서 시작됨.',app.get('port'));
+});
---
+++ Study_Code/app5.js
@@ -0,0 +1,16 @@
+var express = require('express')
+, http = require('http');
+
+var app = express();
+
+app.use(function(req, res, next){
+    console.log('첫 번째 미들웨어에서 요청을 처리함.');
+    
+    res.redirect('http://google.co.kr');
+});
+
+app.set('port',process.env.PORT || 2402);
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express 서버가 %d번 포트에서 시작됨.',app.get('port'));
+});
